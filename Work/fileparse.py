9997c433d84badec0440fefa32890f62f7fb6986
vincentlegoualher <v.legoualher@gmail.com>
2020-06-10 11:12:21 +0200

    fileparse

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,3 +1,42 @@
 # fileparse.py
 #
 # Exercise 3.3
+
+import csv
+
+def parse_csv(filename, selection = None, types = [], has_headers = True, delim = ',' ):
+    '''
+    Parse a .csv file into a list of records
+    '''
+    with open(filename) as f:
+        rows = csv.reader(f, delimiter = delim)
+
+        # File structure
+        if has_headers:
+            headers = next(rows)
+        else:
+            headers = []
+            
+        if selection:
+            indices = [headers.index(colname) for colname in selection]
+            headers = selection
+
+        # Rows
+        records = []
+        for row in rows:
+            if not row:
+                continue
+
+            if selection:
+                row = [row[index] for index in indices]
+            if types:
+                row = [fun(val) for fun,val in zip(types, row)]
+            if headers:
+                record = dict(zip(headers, row))
+            else:
+                record = tuple(row)
+
+            records.append(record)
+        
+        return records
+
